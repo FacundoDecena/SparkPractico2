27e21279426c497845a7d89d4008e61803f1bd76
Facundo Decena <facundo.decena@gmail.com>
2020-04-01 22:48:31 -0300

    Prevented radio <= 0

--- Ejercicio4/src/main/java/circunferencia/Circunferencia.java
+++ Ejercicio4/src/main/java/circunferencia/Circunferencia.java
@@ -1,10 +1,14 @@
 package circunferencia;
 
 public class Circunferencia {
-    public double perimetro(double r){
+    public double perimetro(double r) throws NumberFormatException{
+        if (r < 0.01)
+            throw new NumberFormatException("Numero menor al minimo");
         return 2 * Math.PI * r;
     }
-    public double area(double r){
+    public double area(double r) throws NumberFormatException{
+        if (r < 0.01)
+            throw new NumberFormatException("Numero menor al minimo");
         return Math.PI * r * r;
     }
 }
